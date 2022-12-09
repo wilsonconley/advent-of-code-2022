0eb75b600b0e49b63283eb992c88fe832075d778
wilsonconley <conleywilson0@gmail.com>
2022-12-09 15:22:43 -0500

    Day 4

---
+++ code/day4.py
@@ -0,0 +1,27 @@
+def main() -> None:
+    full_overlap = 0
+    any_overlap = 0
+    with open("inputs/day4") as file:
+        for line in file:
+            contents = line.strip()
+            elf_one, elf_two = contents.split(",")
+            one_min, one_max = [int(x) for x in elf_one.split("-")]
+            two_min, two_max = [int(x) for x in elf_two.split("-")]
+            if (one_min >= two_min and one_max <= two_max) or (
+                two_min >= one_min and two_max <= one_max
+            ):
+                full_overlap += 1
+            if (
+                (one_min >= two_min and one_min <= two_max)
+                or (one_max >= two_min and one_max <= two_max)
+            ) or (
+                (two_min >= one_min and two_min <= one_max)
+                or (two_max >= one_min and two_max <= one_max)
+            ):
+                any_overlap += 1
+    print(f"Num full_overlap pairs = {full_overlap}")
+    print(f"Num any_overlap pairs = {any_overlap}")
+
+
+if __name__ == "__main__":
+    main()
