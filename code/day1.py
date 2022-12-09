783035573eb11ce49b60258a60f1f632d470fbb8
wilsonconley <conleywilson0@gmail.com>
2022-12-09 11:51:10 -0500

    Day 1

--- code/day1.py
+++ code/day1.py
@@ -1,5 +1,25 @@
-import typing
+class Elf:
+    items: list[int]
+    total: int = 0
+
+    def __init__(self, items: list[int]) -> None:
+        self.items = items
+        self.total = sum(items)
 
 
 def main() -> None:
-    pass
+    elves: list[Elf] = []
+    items: list[int] = []
+    for line in open("inputs/day1"):
+        line_ = line.strip()
+        if line_:
+            items.append(int(line_))
+        else:
+            elves.append(Elf(items))
+            items = []
+    print("max is: " + str(max([elf.total for elf in elves])))
+    print("top 3: " + str(sum(sorted([elf.total for elf in elves], reverse=True)[:3])))
+
+
+if __name__ == "__main__":
+    main()
